536ef203eeec5aa2561d1968041c179847c281aa
Pham Luu <phamthithanhluu1990@gmail.com>
2025-12-17 12:27:12 +0700

    delete file

--- server.py
+++
@@ -1,9 +0,0 @@
-import uvicorn
-from app import app
-import os
-
-if __name__ == "__main__":
-    uvicorn.run("app:app",
-                host="127.0.0.1",
-                port=int(os.getenv("PORT", 8000)),
-                reload=True)
